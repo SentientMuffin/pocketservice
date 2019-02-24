c5395e324f3ab947efa5ef54c903616750a25d3d
PowWow <powelllxj@gmail.com>
2019-02-24 15:00:48 -0500

    added users application and the user model

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+    phone_number = models.BigIntegerField(blank=True)
