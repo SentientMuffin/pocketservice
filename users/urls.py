f633207ecd3e7b3db8ec8b979cc65341833a210c
PowWow <powelllxj@gmail.com>
2019-02-24 20:57:41 -0500

    allowed view details of user model

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     #     name='account_creation',)
     path('users/create', views.UserList.as_view()),
     path('users/', views.UserList.as_view()),
-    path('users/<int:id>/', views.UserDetail.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
